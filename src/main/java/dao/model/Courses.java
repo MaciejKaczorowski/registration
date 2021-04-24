9678b28dabeb3aed9a850801881f0e4a6a6101f9
Maciej Kaczorowski <maciejfkaczorowski@gmail.com>
2021-04-24 14:36:26 +0200

    Employees, Courses

--- src/main/java/dao/model/Courses.java
+++ src/main/java/dao/model/Courses.java
@@ -1,4 +1,62 @@
 package dao.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table (name = "courses")
 public class Courses {
+
+    @Id
+    @Column(name = "course_name")
+    private String courseName;
+    @Column(name = "course_description")
+    private String courseDescription;
+    @Column(name = "teacher_id")
+    private int teacherId;
+
+    List<Courses> courses = new ArrayList<>();
+
+    public void addCourse (Courses course) {
+        courses.add(course);
+    }
+
+
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getCourseDescription() {
+        return courseDescription;
+    }
+
+    public void setCourseDescription(String courseDescription) {
+        this.courseDescription = courseDescription;
+    }
+
+    public int getTeacherId() {
+        return teacherId;
+    }
+
+    public void setTeacherId(int teacherId) {
+        this.teacherId = teacherId;
+    }
+
+    @Override
+    public String toString() {
+        return "Courses{" +
+                "courseName='" + courseName + '\'' +
+                ", courseDescription='" + courseDescription + '\'' +
+                ", teacherId=" + teacherId +
+                '}';
+    }
 }
